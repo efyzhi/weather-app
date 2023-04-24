5b7a99ca456a4eb2b43bc7bb731b1f9b4341c250
efyzhi <pelurobert10@gmail.com>
2023-04-24 17:25:33 +0100

    up

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
         <Image
           src="https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
           layout="fill"
-          className="object-cover"
+          className="object-fill"
         />
 
         <div className="relative flex justify-between item-center max-w-[500px] w-[100%] m-auto pt-4 px-4 text-white z-10">
